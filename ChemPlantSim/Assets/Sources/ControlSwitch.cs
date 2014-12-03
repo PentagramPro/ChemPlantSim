03ddec62dd4cee8494bf651612a6695f26fa1041
PentagramPro <maxon-line@yandex.ru>
2014-12-03 03:43:56 +0300

    some coding just to remind Unity

---
+++ ChemPlantSim/Assets/Sources/ControlSwitch.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class ControlSwitch : MonoBehaviour{
+
+
+	public Animator SwitchAnimator;
+	bool turnedOn = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public bool IsTurnedOn()
+	{
+		if(SwitchAnimator.GetCurrentAnimatorStateInfo(0).IsName("TurnedOn"))
+			return true;
+		return false;
+	}
+
+	public void OnSwitchClick()
+	{
+
+		if(turnedOn)
+			SwitchAnimator.SetTrigger("TurnOff");
+		else 
+			SwitchAnimator.SetTrigger("TurnOn");
+		turnedOn = !turnedOn;
+	}
+}
